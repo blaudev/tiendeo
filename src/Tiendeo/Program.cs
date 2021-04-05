0e3c461875c53a26c46d6617ae8a77db3d6cd9e6
Daniel Peiró <danielpeiro72@gmail.com>
2021-04-05 10:38:33 +0200

    Pasamos el área como argumento de línea de comandos y si no lanzamos excepción

--- src/Tiendeo/Program.cs
+++ src/Tiendeo/Program.cs
@@ -9,10 +9,25 @@ namespace Tiendeo
     {
         public static async Task Main(string[] args)
         {
+            if (args is null || args.Length is not 2)
+            {
+                throw new ArgumentException("'area width' and 'area height' are required");
+            }
+
+            if (int.TryParse(args[0], out int areaWidth) is false || areaWidth is not > 1)
+            {
+                throw new ArgumentException("'area width' must be a valid number");
+            }
+
+            if (int.TryParse(args[1], out int areaHeight) is false || areaHeight is not > 1)
+            {
+                throw new ArgumentException("'area height' must be a valid number");
+            }
+
             IDataProvider dataProvider = new TestDataProvider();
 
             var app = new App(dataProvider);
-            await app.RunAsync(5, 5);
+            await app.RunAsync(areaWidth, areaHeight);
         }
     }
 }
