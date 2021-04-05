7bc71f3b777634c65b047e2cc05e21f5dc456e62
Daniel Peiró <danielpeiro72@gmail.com>
2021-04-05 11:02:06 +0200

    Saneamos la entrada de datos y la convertimos en una lista de datos

--- src/Tiendeo/App.cs
+++ src/Tiendeo/App.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 using Tiendeo.DataProviders;
 using Tiendeo.DronesManagers;
+using Tiendeo.Models;
 
 namespace Tiendeo
 {
--- src/Tiendeo/DronesManagers/SimpleDronesManager.cs
+++ src/Tiendeo/DronesManagers/SimpleDronesManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 using Tiendeo.Models;
 
@@ -8,7 +9,29 @@ namespace Tiendeo.DronesManagers
     {
         public List<Drone> CreateDrones(string data)
         {
-            throw new System.NotImplementedException();
+            if (string.IsNullOrEmpty(data))
+            {
+                throw new ArgumentException("data must be a valid data");
+            }
+
+            var drones = new List<Drone>();
+
+            var dataList = PrepareData(data);
+
+            return drones;
+        }
+
+        /// <summary>
+        /// Remove line breaks and returns only drones data
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private string[] PrepareData(string data)
+        {
+            data = data.Replace("\r\n", "|").Trim('|');
+            var dronesdata = data.Split('|')[1..];
+
+            return dronesdata;
         }
     }
 }
