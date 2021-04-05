6c7edfc63b993f05d8277b3d5319356834b4ef23
Daniel Peiró <danielpeiro72@gmail.com>
2021-04-05 16:04:01 +0200

    Últimos retoques

--- src/Tiendeo/DronesManagers/IDronesManager.cs
+++ src/Tiendeo/DronesManagers/IDronesManager.cs
@@ -7,7 +7,18 @@ namespace Tiendeo.DronesManagers
 {
     public interface IDronesManager
     {
+        /// <summary>
+        /// Create drones and their position, direction and actions
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         List<Drone> CreateDrones(string data);
+
+        /// <summary>
+        /// Execute all drone actions and update his position and direction
+        /// </summary>
+        /// <param name="drone"></param>
+        /// <returns></returns>
         Task<Drone> ExecuteActionsAsync(Drone drone);
     }
 }
